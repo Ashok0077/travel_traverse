7c6f70535ca8113345f25cc314c0a8627fb686c6
Ashok0077 <vaishnavashok7297@gmail.com>
2023-08-28 18:49:38 +0530

    backend code adeed!!!!

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -10,4 +10,122 @@ export const createTour = async(req,res)=>{
     } catch (err) {
         res.status(500).json({success:false,message:'failed to create. Try again'})
     }
+};
+
+//update tour
+export const updateTour = async (req,res)=>{
+
+    const id=req.params.id
+
+    try {
+
+        const updateTour = await Tour.findByIdAndUpdate(id,{
+            $set:req.body
+        },{new:true})
+
+        res.status(200).json({success:true,message:'successfully updated',data:updateTour})
+        
+    } catch (err) {
+
+        res.status(500).json({success:false,message:'failed to update'})
+        
+    }
+};
+
+export const deleteTour = async (req,res)=>{
+    const id=req.params.id
+
+    try {
+
+        await Tour.findByIdAndDelete(id)
+
+        res.status(200).json({success:true,message:'successfully deleted'})
+        
+    } catch (err) {
+
+        res.status(500).json({success:false,message:'failed to delete'})
+        
+    }
+};
+
+export const getSingleTour = async (req,res)=>{
+    const id=req.params.id
+
+    try {
+
+        const tour = await Tour.findById(id)
+
+        res.status(200).json({success:true,message:'successful',data:tour})
+        
+    } catch (err) {
+
+        res.status(404).json({success:false,message:'failed to find'})
+        
+    }
+};
+
+export const getAllTour = async (req,res)=>{
+
+    // for pagination
+    const page = parseInt(req.query.page);
+    
+    console.log(page);
+
+    try {
+
+        const tours = await Tour.find({}).skip(page*8).limit(8);
+
+        res.status(200).json({success:true, count:tours.length, message:'successful',data:tours})
+        
+    } catch (err) {
+
+        res.status(404).json({success:false,message:'failed to find'})
+        
+    }
+};
+
+//get tour by search
+export const getTourBySearch = async(req,res)=>{
+    const city = new RegExp(req.query.city, 'i')
+    const distance = parseInt(req.query.distance)
+    const maxGroupSize = parseInt(req.query.maxGroupSize)
+
+    try {
+
+        const tours = await Tour.find({ city, distance:{$gte:distance}, maxGroupSize:{ $gte: maxGroupSize },})
+
+        res.status(200).json({success:true,message:'successful',data:tours})
+        
+    } catch (err) {
+
+        res.status(404).json({success:false,message:'failed to find'})
+        
+    }
+}
+
+
+export const getFeaturedTour = async (req,res)=>{
+
+
+    try {
+
+        const tours = await Tour.find({featured:true}).limit(8);
+
+        res.status(200).json({success:true, count:tours.length, message:'successful',data:tours})
+        
+    } catch (err) {
+
+        res.status(404).json({success:false,message:'failed to find'})
+        
+    }
+};
+
+export const getTourCount = async(req,res)=>{
+    try {
+        const tourCount = await Tour.estimatedDocumentCount();
+
+        res.status(200).json({success:true, data:tourCount})
+    } catch (err) {
+        res.status(500).jason({success:false, message: "failed to fetch"})
+    }
 }
