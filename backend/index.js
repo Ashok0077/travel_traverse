5f1cee542d8917f2d5f6a0df6b633f689e97c9ad
Ashok Vaishnav <88554526+Ashok0077@users.noreply.github.com>
2024-06-29 17:43:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,22 +33,23 @@ app.use(cors({
 }));
 
 
-mongoose.set("strictQuery", false);
-
-export const connect = async (req, res) => {
+mongoose.set("strictQuery", false)
+const connect=async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        })
 
         console.log('MongoDB database connected');
-        res.json({ message: 'MongoDB database connected' });
+        
     } catch (err) {
-        console.log('MongoDB database connection failed');
-        res.status(500).json({ message: 'MongoDB database connection failed' });
+
+        console.log('MongoDB database connection failed')
+        
     }
-};
+} 
 
 //middleware
 app.use(express.json())
