a3bbc7a8abcfa0b540fd41646e5b00b133fbbee2
Ashok Vaishnav <88554526+Ashok0077@users.noreply.github.com>
2024-06-29 12:03:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,10 +21,16 @@ const corsOptions = {
 }
 
 
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true
+// }));
+
 app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
+         origin: 'https://travel-traverse.vercel.app',
+         credentials: true
 }));
+
 
 mongoose.set("strictQuery", false)
 const connect=async()=>{
