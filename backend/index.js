e1aae183ba8605b007c15180e8422ffd4f0eb4d4
Ashok0077 <vaishnavashok7297@gmail.com>
2024-06-29 11:58:51 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import tourRoute from './routes/tours.js'
 import userRoute from './routes/users.js'
 import authRoute from './routes/auth.js'
 import reviewRoute from "./routes/reviews.js"
-import bookingRoute from "./routes/booking.js"
+import bookingRoute from "./routes/booking.js" //roter ka kuch bhi naam de sakte hai
 
 
 
@@ -21,9 +21,14 @@ const corsOptions = {
 }
 
 
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true
+// }));
+
 app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
+         origin: 'https://travel-traverse.vercel.app',
+         credentials: true
 }));
 
 mongoose.set("strictQuery", false)
@@ -44,12 +49,24 @@ const connect=async()=>{
     }
 }
 
+//how to connect to mongoDb
+//first built mongoDb schema
+//then make model
+//connect mongoDb with our backend server
+
+//mongodb represent data in documnent form, mongoDb have collection instead of table, in collection there are document
+
+//connecting mongoDb
+//mongoose.connect("mongoDb_url")
+//.then(()=>console.log("MongoDB connected"))
+//.catch((err)=>console.log("mongo error",err));
+
 
 //middleware
 app.use(express.json())
 app.use(cors(corsOptions))
 app.use(cookieParser())
-app.use('/api/v1/auth', authRoute)
+app.use('/api/v1/auth', authRoute) //jab /api/v1/auth pe koi request aate hai tho authRoute ka istemal karna
 app.use('/api/v1/tours', tourRoute)
 app.use('/api/v1/users', userRoute)
 app.use('/api/v1/review', reviewRoute)
